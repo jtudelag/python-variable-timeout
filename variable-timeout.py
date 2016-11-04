718f0de4221ce3b8f32eecef04fd29d9b6127ab2
Jorge Tudela <jtudelag@redhat.com>
2016-11-04 16:14:35 +0100

    Adds sample app and requirements.

---
+++ variable-timeout.py
@@ -0,0 +1,18 @@
+from bottle import route, run, template
+import time, math
+
+TIMEOUT=5
+
+@route('/hello')
+def index():
+    time.sleep(int(TIMEOUT))
+    return template('<b>Hello world after {{timeout}}</b>!', timeout=TIMEOUT)
+
+@route('/timeout/<to>')
+def setTimeout(to):
+    global TIMEOUT
+    TIMEOUT=int(to)
+    return template('<b>Timeout set to: {{timeout}}</b>!', timeout=to)
+
+run(host='0.0.0.0', port=8080)
+
